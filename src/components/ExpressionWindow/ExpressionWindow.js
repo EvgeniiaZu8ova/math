fed1056c9e41ff627fbea508bdedf6768c9b7ac2
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-08-14 00:02:49 +0400

    feat: add buttons and page number

--- src/components/ExpressionWindow/ExpressionWindow.js
+++ src/components/ExpressionWindow/ExpressionWindow.js
@@ -1,15 +1,60 @@
-import React from "react";
+import { React, useState } from "react";
 import "./ExpressionWindow.css";
+
+import arrow from "../../images/arrow_back-symbol.svg";
 
 import Expression from "./Expression/Expression";
 
-function ExpressionWindow(props) {
+function ExpressionWindow() {
+  const [page, setPage] = useState(1);
+  const [isFirst, setIsFirst] = useState(true);
+
+  function handleClickNext() {
+    setIsFirst(false);
+    setPage((prev) => {
+      if (prev < 9) {
+        return prev + 1;
+      } else {
+        return prev;
+      }
+    });
+  }
+
+  function handleClickBack() {
+    setPage((prev) => {
+      if (prev > 1) {
+        return prev - 1;
+      } else {
+        return prev;
+      }
+    });
+  }
+
   return (
     <div className="expr-window__container">
       <h1 className="expr-window__title">
         В итоге получается следующая формула:
       </h1>
       <Expression />
+      <div className="expr-window__bottom">
+        <p className="expr-window__page-counter">{`${page} из 9`}</p>
+        <div className="expr-window__buttons">
+          <button
+            onClick={handleClickBack}
+            className={`expr-window__button ${
+              isFirst && "expr-window__button_hidden"
+            }`}
+          >
+            <img src={arrow} alt="Назад" className="expr-window__icon" />
+          </button>
+          <button
+            onClick={handleClickNext}
+            className="expr-window__button expr-window__button_black"
+          >
+            Далее
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
